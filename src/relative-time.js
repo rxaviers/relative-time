03fc410a86a5f87cfbcc53c23aff45f6fb735424
Rafael Xavier de Souza <rxavier@paypal.com>
2017-03-29 16:38:04 -0300

    New algorithm, customizable thresholds & accepts explicit options.units

--- src/relative-time.js
+++ src/relative-time.js
@@ -1,37 +1,41 @@
-/**
- * Rule: (equivalent for future)
- *
- * 0s - now
- * ]0s, 1min[ - x seconds ago
- * [1min, 2min[ - 1 minute ago
- * [xmin, x+1min[ - x minutes ago
- * [1h, 2h[ - 1 hour ago
- * [xh, x+1h[ - x hours ago
- * [xh, x+1h[ - yesterday if x >= 8 and range is on different days.
- * [1d, 2d[ - yesterday
- * [xd, x+1d[ - x days ago
- * [xd, x+1d[ - last month if x >= 7 and range is on different months.
- * [1month, 2month[ - last month
- * [xmonth, x+1month[ - x months ago
- * [xmonth, x+1month[ - last year if x >= 3 and range is on different years.
- * [1y, 2y[ - last year
- * [xy, x+1y[ - x years ago
- *
- */
 import Globalize from "globalize";
 
 const second = 1e3;
 const minute = 6e4;
 const hour = 36e5;
 const day = 864e5;
-const month = 2592e6;
+// const week = 6048e5;
+// const month = 2592e6;
 
-const _6hours = 6 * hour;
-const _7days = 7 * day;
-const _3months = 3 * month;
+function defineCachedGetter(obj, prop, get) {
+  defineGetter(obj, prop, function() {
+    if (!this._[prop]) {
+      this._[prop] = get.call(this);
+    }
+    return this._[prop];
+  });
+}
 
-function daysInMonth(date) {
-  return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+function defineGetter(obj, prop, get) {
+  Object.defineProperty(obj, prop, {get});
+}
+
+function startOf(date, unit) {
+  date = new Date(date.getTime());
+  switch (unit) {
+    case "year": date.setMonth(0);
+    // falls through
+    case "month": date.setDate(1);
+    // falls through
+    case "day": date.setHours(0);
+    // falls through
+    case "hour": date.setMinutes(0);
+    // falls through
+    case "minute": date.setSeconds(0);
+    // falls through
+    case "second": date.setMilliseconds(0);
+  }
+  return date;
 }
 
 export default class RelativeTime {
@@ -39,55 +43,93 @@ export default class RelativeTime {
     this.formatters = RelativeTime.initializeFormatters(...arguments);
   }
 
-  format(date) {
+  format(date, {unit = "best-fit"} = {}) {
     var formatters = this.formatters;
     var now = new Date();
-    var diff = date.getTime() - now.getTime();
-    var absDiff = Math.abs(diff);
-    var round = Math[diff > 0 ? "floor" : "ceil"];
-    var diffYears = date.getFullYear() - now.getFullYear();
-    var diffMonths = date.getMonth() - now.getMonth();
-    var diffDays = date.getDate() - now.getDate();
-    if (diffYears) {
-      diffMonths += date > now ? 12 : -12;
-    }
-    if (diffMonths) {
-      diffDays += date > now ? daysInMonth(now) : -daysInMonth(date);
+    var diff = {
+      _: {},
+      ms: date.getTime() - now.getTime(),
+      years: date.getFullYear() - now.getFullYear()
+    };
+    var round = Math[diff.ms > 0 ? "floor" : "ceil"];
+
+    defineCachedGetter(diff, "months", function() {
+      return this.years * 12 + date.getMonth() - now.getMonth();
+    });
+    defineCachedGetter(diff, "days", function() {
+      return round((startOf(date, "day") - startOf(now, "day")) / day);
+    });
+    defineCachedGetter(diff, "hours", function() {
+      return round((startOf(date, "hour") - startOf(now, "hour")) / hour);
+    });
+    defineCachedGetter(diff, "minutes", function() {
+      return round((startOf(date, "minute") - startOf(now, "minute")) / minute);
+    });
+    defineCachedGetter(diff, "seconds", function() {
+      return round((startOf(date, "second") - startOf(now, "second")) / second);
+    });
+
+    var absDiff = {
+      _: {}
+    };
+
+    defineGetter(absDiff, "years", function() {
+      return Math.abs(diff.years);
+    });
+    defineGetter(absDiff, "months", function() {
+      return Math.abs(diff.months);
+    });
+    defineGetter(absDiff, "days", function() {
+      return Math.abs(diff.days);
+    });
+    defineGetter(absDiff, "hours", function() {
+      return Math.abs(diff.hours);
+    });
+    defineGetter(absDiff, "minutes", function() {
+      return Math.abs(diff.minutes);
+    });
+    defineGetter(absDiff, "seconds", function() {
+      return Math.abs(diff.seconds);
+    });
+
+    if (unit === "best-fit") {
+      unit = RelativeTime.bestFit(absDiff);
     }
 
-    // now or x seconds ago.
-    if (absDiff < minute) {
-      return formatters.second(round(diff / second));
+    switch(unit) {
+      case "year": return formatters.year(diff.years);
+      case "month": return formatters.month(diff.months);
+      // case "week": return formatters.week(diff.weeks);
+      case "day": return formatters.day(diff.days);
+      case "hour": return formatters.hour(diff.hours);
+      case "minute": return formatters.minute(diff.minutes);
+      default: return formatters.second(diff.seconds);
     }
-
-    // x minutes ago.
-    if (absDiff < hour) {
-      return formatters.minute(round(diff / minute));
-    }
-
-    // x hours ago or yesterday.
-    if (absDiff < day) {
-      if (absDiff > _6hours && diffDays) {
-        return formatters.day(diffDays);
-      }
-      return formatters.hour(round(diff / hour));
-    }
-
-    // x days ago or last month
-    if (absDiff < month) {
-      if (absDiff > _7days && diffMonths) {
-        return formatters.month(diffMonths);
-      }
-      return formatters.day(diffDays);
-    }
-
-    // x months ago or last year or x years ago
-    if (absDiff > _3months && diffYears) {
-      return formatters.year(diffYears);
-    }
-    return formatters.month(diffMonths);
   }
 }
+
+RelativeTime.bestFit = function(absDiff) {
+  let threshold = this.threshold;
+  switch(true) {
+    case absDiff.years > 0 && absDiff.months >= threshold.month: return "year";
+    case absDiff.months > 0 && absDiff.days >= threshold.day: return "month";
+    // case absDiff.months > 0 && absDiff.weeks >= threshold.week: return "month";
+    // case absDiff.weeks > 0 && absDiff.days >= threshold.day: return "week";
+    case absDiff.days > 0 && absDiff.hours >= threshold.hour: return "day";
+    case absDiff.hours > 0 && absDiff.minutes >= threshold.minute: return "hour";
+    case absDiff.minutes > 0 && absDiff.seconds >= threshold.second: return "minute";
+    default: return "second";
+  }
+};
+
+RelativeTime.threshold = {
+  month: 3, // at least 3 months before using year.
+  // week: 4, // at least 4 weeks before using month.
+  day: 7, // at least 7 days before using month.
+  hour: 7, // at least 7 hours before using day.
+  minute: 60, // at least 60 minutes before using hour.
+  second: 60 // at least 60 seconds before using minute.
+};
 
 // TODO: Remove redundancy. The only reason this code is that ugly is to get
 // supported by globalize-compiler (which reads the static formatters).
--- test/relative-time.js
+++ test/relative-time.js
@@ -16,80 +16,115 @@ describe("relative-time", function() {
     clock.restore();
   });
 
-  it("should format seconds distant dates", function() {
-    expect(relativeTime.format(new Date("2016-04-10 11:59:01"))).to.equal("59 seconds ago");
-    expect(relativeTime.format(new Date("2016-04-10 12:00:00"))).to.equal("now");
-    expect(relativeTime.format(new Date("2016-04-10 12:00:59"))).to.equal("in 59 seconds");
+  describe("bestFit", function() {
+    it("should format seconds-distant dates", function() {
+      expect(relativeTime.format(new Date("2016-04-10 11:59:01"))).to.equal("59 seconds ago");
+      expect(relativeTime.format(new Date("2016-04-10 12:00:00"))).to.equal("now");
+      expect(relativeTime.format(new Date("2016-04-10 12:00:59"))).to.equal("in 59 seconds");
+    });
+
+    it("should format minutes-distant dates", function() {
+      expect(relativeTime.format(new Date("2016-04-10 11:01:00"))).to.equal("59 minutes ago");
+      expect(relativeTime.format(new Date("2016-04-10 11:59"))).to.equal("1 minute ago");
+      expect(relativeTime.format(new Date("2016-04-10 12:01"))).to.equal("in 1 minute");
+      expect(relativeTime.format(new Date("2016-04-10 12:01:59"))).to.equal("in 1 minute");
+      expect(relativeTime.format(new Date("2016-04-10 12:59:59"))).to.equal("in 59 minutes");
+    });
+
+    it("should format hours-distant dates", function() {
+      expect(relativeTime.format(new Date("2016-04-10 00:00"))).to.equal("12 hours ago");
+      expect(relativeTime.format(new Date("2016-04-10 13:00"))).to.equal("in 1 hour");
+      expect(relativeTime.format(new Date("2016-04-10 13:59:59"))).to.equal("in 1 hour");
+      expect(relativeTime.format(new Date("2016-04-10 23:59:59"))).to.equal("in 11 hours");
+
+      sinon.useFakeTimers(new Date("2016-04-10 01:00").getTime());
+      expect(relativeTime.format(new Date("2016-04-09 19:00"))).to.equal("6 hours ago");
+      expect(relativeTime.format(new Date("2016-04-09 18:00"))).to.equal("yesterday");
+
+      sinon.useFakeTimers(new Date("2016-04-10 23:00").getTime());
+      expect(relativeTime.format(new Date("2016-04-11 05:00"))).to.equal("in 6 hours");
+      expect(relativeTime.format(new Date("2016-04-11 06:00"))).to.equal("tomorrow");
+
+      sinon.useFakeTimers(new Date("2016-01-31 23:00").getTime());
+      expect(relativeTime.format(new Date("2016-02-01 05:00"))).to.equal("in 6 hours");
+      expect(relativeTime.format(new Date("2016-02-01 07:00"))).to.equal("tomorrow");
+
+      sinon.useFakeTimers(new Date("2016-12-31 23:00").getTime());
+      expect(relativeTime.format(new Date("2017-01-01 05:00"))).to.equal("in 6 hours");
+      expect(relativeTime.format(new Date("2017-01-01 07:00"))).to.equal("tomorrow");
+    });
+
+    it("should format days-distant dates", function() {
+      expect(relativeTime.format(new Date("2016-04-01 00:00"))).to.equal("9 days ago");
+      expect(relativeTime.format(new Date("2016-04-09 18:00"))).to.equal("yesterday");
+      expect(relativeTime.format(new Date("2016-04-11 09:00"))).to.equal("tomorrow");
+      expect(relativeTime.format(new Date("2016-04-30 23:59"))).to.equal("in 20 days");
+      expect(relativeTime.format(new Date("2016-03-31 23:59"))).to.equal("last month");
+      expect(relativeTime.format(new Date("2016-05-01 00:00"))).to.equal("next month");
+
+      sinon.useFakeTimers(new Date("2016-04-06 12:00").getTime());
+      expect(relativeTime.format(new Date("2016-03-31 23:59"))).to.equal("6 days ago");
+
+      sinon.useFakeTimers(new Date("2016-04-25 23:00").getTime());
+      expect(relativeTime.format(new Date("2016-05-01 00:00"))).to.equal("in 6 days");
+    });
+
+    it("should format months-distant dates", function() {
+      expect(relativeTime.format(new Date("2016-01-01 00:00"))).to.equal("3 months ago");
+      expect(relativeTime.format(new Date("2016-03-01 00:00"))).to.equal("last month");
+      expect(relativeTime.format(new Date("2016-05-01 00:00"))).to.equal("next month");
+      expect(relativeTime.format(new Date("2016-11-31 23:59"))).to.equal("in 8 months");
+
+      sinon.useFakeTimers(new Date("2017-01-12 18:30").getTime());
+      expect(relativeTime.format(new Date("2016-12-29 18:30"))).to.equal("last month");
+
+      sinon.useFakeTimers(new Date("2016-12-29 18:30").getTime());
+      expect(relativeTime.format(new Date("2017-01-12 18:30"))).to.equal("next month");
+
+      sinon.useFakeTimers(new Date("2016-02-28 12:00").getTime());
+      expect(relativeTime.format(new Date("2015-12-31 23:59"))).to.equal("2 months ago");
+    });
+
+    it("should format years-distant dates", function() {
+      expect(relativeTime.format(new Date("2010-06-01 12:00"))).to.equal("6 years ago");
+      expect(relativeTime.format(new Date("2015-12-31 23:59"))).to.equal("last year");
+      expect(relativeTime.format(new Date("2017-01-01 00:00"))).to.equal("next year");
+
+      sinon.useFakeTimers(new Date("2016-10-02 12:00").getTime());
+      expect(relativeTime.format(new Date("2017-01-01 00:00"))).to.equal("next year");
+    });
   });
 
-  it("should format minutes distant dates", function() {
-    expect(relativeTime.format(new Date("2016-04-10 11:00:01"))).to.equal("59 minutes ago");
-    expect(relativeTime.format(new Date("2016-04-10 11:59"))).to.equal("1 minute ago");
-    expect(relativeTime.format(new Date("2016-04-10 12:01"))).to.equal("in 1 minute");
-    expect(relativeTime.format(new Date("2016-04-10 12:01:59"))).to.equal("in 1 minute");
-    expect(relativeTime.format(new Date("2016-04-10 12:59:59"))).to.equal("in 59 minutes");
-  });
+  describe("explicit units", function() {
+    it("shold format relative time using seconds", function() {
+      expect(relativeTime.format(new Date("2016-04-10 11:59:01"), "second")).to.equal("59 seconds ago");
+      expect(relativeTime.format(new Date("2016-04-10 11:01:00"), "second")).to.equal("3,540 seconds ago");
+      expect(relativeTime.format(new Date("2016-04-10 00:00"), "second")).to.equal("43,200 seconds ago");
+    });
 
-  it("should format hours distant dates", function() {
-    expect(relativeTime.format(new Date("2016-04-10 00:00"))).to.equal("12 hours ago");
-    expect(relativeTime.format(new Date("2016-04-10 13:00"))).to.equal("in 1 hour");
-    expect(relativeTime.format(new Date("2016-04-10 13:59:59"))).to.equal("in 1 hour");
-    expect(relativeTime.format(new Date("2016-04-10 23:59:59"))).to.equal("in 11 hours");
+    it("shold format relative time using minutes", function() {
+      expect(relativeTime.format(new Date("2016-04-10 12:00:47"), "minute")).to.equal("this minute");
+      expect(relativeTime.format(new Date("2016-04-10 11:59:45"), "minute")).to.equal("1 minute ago");
+      expect(relativeTime.format(new Date("2016-04-10 11:01:00"), "minute")).to.equal("59 minutes ago");
+      expect(relativeTime.format(new Date("2016-04-01 00:00"), "minute")).to.equal("13,680 minutes ago");
+    });
 
-    sinon.useFakeTimers(new Date("2016-04-10 01:00").getTime());
-    expect(relativeTime.format(new Date("2016-04-09 19:00"))).to.equal("6 hours ago");
-    expect(relativeTime.format(new Date("2016-04-09 18:00"))).to.equal("yesterday");
+    it("shold format relative time using hours", function() {
+      expect(relativeTime.format(new Date("2016-04-10 12:45:00"), "hour")).to.equal("this hour");
+      expect(relativeTime.format(new Date("2016-04-10 11:01:00"), "hour")).to.equal("1 hour ago");
+      expect(relativeTime.format(new Date("2016-04-10 00:00"), "hour")).to.equal("12 hours ago");
+      expect(relativeTime.format(new Date("2016-04-01 00:00"), "hour")).to.equal("228 hours ago");
+      expect(relativeTime.format(new Date("2016-01-01 00:00"), "hour")).to.equal("2,413 hours ago");
+    });
 
-    sinon.useFakeTimers(new Date("2016-04-10 23:00").getTime());
-    expect(relativeTime.format(new Date("2016-04-11 05:00"))).to.equal("in 6 hours");
-    expect(relativeTime.format(new Date("2016-04-11 06:00"))).to.equal("tomorrow");
+    it("shold format relative time using days", function() {
+      expect(relativeTime.format(new Date("2016-04-10 11:30:00"), "day")).to.equal("today");
+      expect(relativeTime.format(new Date("2016-01-01 00:00"), "day")).to.equal("100 days ago");
+    });
 
-    sinon.useFakeTimers(new Date("2016-01-31 23:00").getTime());
-    expect(relativeTime.format(new Date("2016-02-01 05:00"))).to.equal("in 6 hours");
-    expect(relativeTime.format(new Date("2016-02-01 07:00"))).to.equal("tomorrow");
-
-    sinon.useFakeTimers(new Date("2016-12-31 23:00").getTime());
-    expect(relativeTime.format(new Date("2017-01-01 05:00"))).to.equal("in 6 hours");
-    expect(relativeTime.format(new Date("2017-01-01 07:00"))).to.equal("tomorrow");
-  });
-
-  it("should format days distant dates", function() {
-    expect(relativeTime.format(new Date("2016-04-01 00:00"))).to.equal("9 days ago");
-    expect(relativeTime.format(new Date("2016-04-09 18:00"))).to.equal("yesterday");
-    expect(relativeTime.format(new Date("2016-04-11 09:00"))).to.equal("tomorrow");
-    expect(relativeTime.format(new Date("2016-04-30 23:59"))).to.equal("in 20 days");
-    expect(relativeTime.format(new Date("2016-03-31 23:59"))).to.equal("last month");
-    expect(relativeTime.format(new Date("2016-05-01 00:00"))).to.equal("next month");
-
-    sinon.useFakeTimers(new Date("2016-04-06 12:00").getTime());
-    expect(relativeTime.format(new Date("2016-03-31 23:59"))).to.equal("6 days ago");
-
-    sinon.useFakeTimers(new Date("2016-04-25 23:00").getTime());
-    expect(relativeTime.format(new Date("2016-05-01 00:00"))).to.equal("in 6 days");
-  });
-
-  it("should format months distant dates", function() {
-    expect(relativeTime.format(new Date("2016-01-01 00:00"))).to.equal("3 months ago");
-    expect(relativeTime.format(new Date("2016-03-01 00:00"))).to.equal("last month");
-    expect(relativeTime.format(new Date("2016-05-01 00:00"))).to.equal("next month");
-    expect(relativeTime.format(new Date("2016-11-31 23:59"))).to.equal("in 8 months");
-
-    sinon.useFakeTimers(new Date("2017-01-12 18:30").getTime());
-    expect(relativeTime.format(new Date("2016-12-29 18:30"))).to.equal("last month");
-
-    sinon.useFakeTimers(new Date("2016-12-29 18:30").getTime());
-    expect(relativeTime.format(new Date("2017-01-12 18:30"))).to.equal("next month");
-
-    sinon.useFakeTimers(new Date("2016-02-28 12:00").getTime());
-    expect(relativeTime.format(new Date("2015-12-31 23:59"))).to.equal("2 months ago");
-  });
-
-  it("should format years distant dates", function() {
-    expect(relativeTime.format(new Date("2010-06-01 12:00"))).to.equal("6 years ago");
-    expect(relativeTime.format(new Date("2015-12-31 23:59"))).to.equal("last year");
-    expect(relativeTime.format(new Date("2017-01-01 00:00"))).to.equal("next year");
-
-    sinon.useFakeTimers(new Date("2016-10-02 12:00").getTime());
-    expect(relativeTime.format(new Date("2017-01-01 00:00"))).to.equal("next year");
+    it("shold format relative time using months", function() {
+      expect(relativeTime.format(new Date("2016-04-10 23:59:59"), "month")).to.equal("this month");
+      expect(relativeTime.format(new Date("2017-01-01 00:00"), "month")).to.equal("in 9 months");
+    });
   });
 });
