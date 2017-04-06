5e09cfcdb871b04ac8865564aa9a09cfc780391b
Rafael Xavier de Souza <rxaviers@gmail.com>
2017-04-06 11:33:50 -0300

    Test: Remove unnecessary setup code

--- test/index.js
+++ test/index.js
@@ -9,9 +9,4 @@ Globalize.load(
   require("cldr-data").entireSupplemental(),
   require("cldr-data").entireMainFor("en")
 );
-Globalize.loadMessages({
-  en: {
-    "just now": "just now"
-  }
-});
 Globalize.locale("en");
