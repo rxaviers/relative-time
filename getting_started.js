b29bd373e71d056abaaae36fd77b7775d1362c5c
Rafael Xavier de Souza <rxaviers@gmail.com>
2025-09-30 07:36:13 -0300

    feat(getting_started): add Temporal polyfill fallback and use dist RelativeTime

--- getting_started.js
+++ getting_started.js
@@ -1,4 +1,10 @@
-const RelativeTime = require("./src/relative-time").default;
+let Temporal = globalThis.Temporal;
+if (!Temporal) {
+  Temporal = require("@js-temporal/polyfill").Temporal;
+  globalThis.Temporal = Temporal;
+}
+
+const RelativeTime = require("./dist/relative-time").default;
 
 const relativeTime = new RelativeTime();
 const now = Temporal.Now.zonedDateTimeISO();
